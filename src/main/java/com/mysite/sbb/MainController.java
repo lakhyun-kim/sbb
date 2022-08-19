af24ed5097f3cbd9ad4f733c2a4a9784771f53c7
lakhyun.k <java2114@naver.com>
2022-08-19 13:38:25 +0900

    MainController 주석 수정

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// 스프링부트의 컨트롤러가 된다.
+// @Controller : 스프링부트의 컨트롤러가 된다.
 @Controller
 public class MainController {
 
-    // 요청된 URL과의 매핑
+    // @RequestMapping : 요청된 URL과의 매핑
     @RequestMapping("/sbb")
-    // URL 요청에 대한 응답으로 문자열을 리턴
+    // @ResponseBody : URL 요청에 대한 응답으로 문자열을 리턴
     @ResponseBody
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
