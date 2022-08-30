9a8af1b5d5a5e7fa87f2e8cc9a7840432acf43ca
lakhyun.k <java2114@naver.com>
2022-08-30 14:09:36 +0900

    3-14 검색

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,14 +1,22 @@
 package com.mysite.sbb.question;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +30,55 @@ public class QuestionService {
     // questionRepository 객체는 생성자 방식으로 DI 규칙에 의해 주입된다.
     private final QuestionRepository questionRepository;
 
+    private Specification<Question> search(String kw) {
+        return new Specification<Question>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);   // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = q.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+                /**
+                 * 해당쿼리를 위와 같이 표현
+                 * select
+                 *     distinct q.id,
+                 *     q.author_id,
+                 *     q.content,
+                 *     q.create_date,
+                 *     q.modify_date,
+                 *     q.subject
+                 * from question q
+                 * left outer join site_user u1 on q.author_id=u1.id
+                 * left outer join answer a on q.id=a.question_id
+                 * left outer join site_user u2 on a.author_id=u2.id
+                 * where
+                 *     q.subject like '%스프링%'
+                 *     or q.content like '%스프링%'
+                 *     or u1.username like '%스프링%'
+                 *     or a.content like '%스프링%'
+                 *     or u2.username like '%스프링%'
+                 */
+            }
+        };
+    }
+
     public List<Question> getList() {
         return this.questionRepository.findAll();
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        /*Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);*/
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
 
     public Question getQuestion(Integer id) {
